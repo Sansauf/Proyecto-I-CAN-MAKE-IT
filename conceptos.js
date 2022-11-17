9f80ccb4cd42f14581cec47b4f7897b385dfa17f
Sansauf <fsaulspuche@gmail.com>
2022-11-17 14:09:18 -0500

    17/11/2022 intro a js

---
+++ conceptos.js
@@ -0,0 +1,19 @@
+// conceptos basicos en javascript
+/*
+declarar las variables en javascript
+1. var   forma estandar, navegadores antiguos
+2. let    si la variable puede cambiar
+3. const   para constantes por el IVA, nomenclaturas, id, etc.
+*/
+
+var nombre = "nombre"; //variable de tipo cadena o texto
+let edad = 45; //variable de tipo entero
+const IVA = 3.14159; //variable tipo flotante
+
+
+// datos de salida o imprimir un dato en la pagina web
+document.write(nombre);
+// console.log en la consola del navegador o devtools
+console.log(nombre)
+
+var edad = prompt("cual es tu edad")
